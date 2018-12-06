15eec121e99c32be20b2b471899b2e95595038a9
ssh <sperfect@gmail.com>
2018-12-06 18:16:14 +0200

    YoutubeApi get list items

--- DjuQBox.net.Server/Program.cs
+++ DjuQBox.net.Server/Program.cs
@@ -16,23 +16,30 @@ namespace DjuQBox.net.Server
         //https://stackoverflow.com/a/14750116/8289048
         static void Main(string[] args)
         {
+
+            new Program().Run("test").Wait();
+
             Console.WriteLine("YouTube Data API: Search");
             Console.WriteLine("========================");
 
             fYoutubeDL = new YoutubeDL();
-            fYoutubeDL.YoutubeDlPath = @"C:\DjQbox\youtube-dl\youtube-dl.exe";
+           
             fYoutubeDL.StandardOutputEvent += (sender, output) => Console.WriteLine(output);
             fYoutubeDL.StandardErrorEvent += (sender, errorOutput) => Console.WriteLine(errorOutput);
 
             
             fYoutubeDL.Options.PostProcessingOptions.PreferFfmpeg = true;
-            fYoutubeDL.Options.PostProcessingOptions.FfmpegLocation = @"C:\DjQbox\youtube-dl\ffmpeg-20170425-b4330a0-win64-static\bin";
+            
             fYoutubeDL.Options.PostProcessingOptions.ExtractAudio = true;
             fYoutubeDL.Options.PostProcessingOptions.AudioFormat = NYoutubeDL.Helpers.Enums.AudioFormat.mp3;
 
             fYoutubeDL.Options.FilesystemOptions.Output = @"C:\DjQbox\mp3s\test.test";
+            fYoutubeDL.YoutubeDlPath = @"C:\DjQbox\youtube-dl\youtube-dl.exe";
+            fYoutubeDL.Options.PostProcessingOptions.FfmpegLocation = @"C:\DjQbox\youtube-dl\ffmpeg-20170425-b4330a0-win64-static\bin";
 
             String _q = Console.ReadLine();
+
+            
 
             DownloadVideo("https://www.youtube.com/watch?v=7WFk23_6yos");
 
@@ -46,6 +53,8 @@ namespace DjuQBox.net.Server
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
 
+            
+
 
             //download
             //linux 
@@ -56,7 +65,7 @@ namespace DjuQBox.net.Server
             //http://cntlm.sourceforge.net/
 
             //https://www.youtube.com/watch?v=7WFk23_6yos
-            
+
         }
 
         static YoutubeDL fYoutubeDL;
@@ -64,12 +73,13 @@ namespace DjuQBox.net.Server
         private static void DownloadVideo(string aVideoId)
         {
             fYoutubeDL.VideoUrl = "https://www.youtube.com/watch?v=7WFk23_6yos";
-            string commandToRun = fYoutubeDL.PrepareDownload();
-            Console.WriteLine(commandToRun);
+            
+            //string commandToRun = fYoutubeDL.PrepareDownload();
+            //Console.WriteLine(commandToRun);
             //https://gitlab.com/BrianAllred/NYoutubeDL
             //.\youtube-dl.exe -v -x -i --proxy "http://localhost:3128" --audio-format mp3 --prefer-ffmpeg  --ffmpeg-location "C:\DjQbox\youtube-dl\ffmpeg-20170425-b4330a0-win64-static\bin" https://www.youtube.com/watch?v=7WFk23_6yos
 
-            
+
             fYoutubeDL.Download();
         }
 
@@ -81,40 +91,70 @@ namespace DjuQBox.net.Server
                 ApplicationName = this.GetType().ToString()
             });
 
-            var searchListRequest = youtubeService.Search.List("snippet");
-            searchListRequest.Q = aQuery; // Replace with your search term.
-            searchListRequest.MaxResults = 50;
+            //var listSearch = youtubeService.PlaylistItems.List("id,contentDetails,snippet");
+            var listSearch = youtubeService.PlaylistItems.List("snippet");
+            listSearch.PlaylistId = "PLSRDGXudTSm9PJfaMl2tq6I5DM--Gh8Is";
+            //listSearch.Key = "AIzaSyBif1qH8cEFwWulm6KAPtwpDik4MvNBm5c";
+            //listSearch.Fields = "items(snippet(resourceId(videoId),publishedAt,title,thumbnails/default/url))";
+            listSearch.MaxResults = 50; // max value!!!!
+            //listSearch.PageToken
 
-            // Call the search.list method to retrieve results matching the specified query term.
-            var searchListResponse = await searchListRequest.ExecuteAsync();
+            List<string> playListVideos = new List<string>();
 
-            List<string> videos = new List<string>();
-            List<string> channels = new List<string>();
-            List<string> playlists = new List<string>();
 
-            // Add each result to the appropriate list, and then display the lists of
-            // matching videos, channels, and playlists.
-            foreach (var searchResult in searchListResponse.Items)
+            
+
+            var nextPageToken = "";
+
+            while (nextPageToken != null)
             {
-                switch (searchResult.Id.Kind)
+                listSearch.PageToken = nextPageToken;
+                var listSearchRes = listSearch.Execute();
+
+                foreach (var listItem in listSearchRes.Items)
                 {
-                    case "youtube#video":
-                        videos.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.VideoId));
-                        break;
-
-                    case "youtube#channel":
-                        channels.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.ChannelId));
-                        break;
-
-                    case "youtube#playlist":
-                        playlists.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.PlaylistId));
-                        break;
+                    playListVideos.Add(String.Format("{0} ({1})", listItem.Snippet.Title, listItem.Snippet.ResourceId.VideoId));
                 }
+
+                nextPageToken = listSearchRes.NextPageToken;
             }
 
-            Console.WriteLine(String.Format("Videos:\n{0}\n", string.Join("\n", videos)));
-            Console.WriteLine(String.Format("Channels:\n{0}\n", string.Join("\n", channels)));
-            Console.WriteLine(String.Format("Playlists:\n{0}\n", string.Join("\n", playlists)));
+            Console.WriteLine(String.Format("Videos:\n{0}\n", string.Join("\n", playListVideos)));
+
+            //var searchListRequest = youtubeService.Search.List("snippet");
+            //searchListRequest.Q = aQuery; // Replace with your search term.
+            //searchListRequest.MaxResults = 100;
+
+            //// Call the search.list method to retrieve results matching the specified query term.
+            //var searchListResponse = await searchListRequest.ExecuteAsync();
+
+            //List<string> videos = new List<string>();
+            //List<string> channels = new List<string>();
+            //List<string> playlists = new List<string>();
+
+            //// Add each result to the appropriate list, and then display the lists of
+            //// matching videos, channels, and playlists.
+            //foreach (var searchResult in searchListResponse.Items)
+            //{
+            //    switch (searchResult.Id.Kind)
+            //    {
+            //        case "youtube#video":
+            //            videos.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.VideoId));
+            //            break;
+
+            //        case "youtube#channel":
+            //            channels.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.ChannelId));
+            //            break;
+
+            //        case "youtube#playlist":
+            //            playlists.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.PlaylistId));
+            //            break;
+            //    }
+            //}
+
+            //Console.WriteLine(String.Format("Videos:\n{0}\n", string.Join("\n", videos)));
+            //Console.WriteLine(String.Format("Channels:\n{0}\n", string.Join("\n", channels)));
+            //Console.WriteLine(String.Format("Playlists:\n{0}\n", string.Join("\n", playlists)));
         }
     }
 }
